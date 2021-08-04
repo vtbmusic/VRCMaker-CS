a9b33ea055df75ed3e979842d953dfb1dd8447fa
Alphaly2K <1990369867@qq.com>
2021-08-04 18:35:32 +0800

    feat($page): add The Navigation of Setup Pages
    添加了初始化页面的导航以及账户设置页面（暂未实装）

--- Pages/Setup.xaml.cs
+++ Pages/Setup.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 using VRCMaker.Pages.SetupPages;
 
 namespace VRCMaker.Pages
@@ -11,11 +13,44 @@ namespace VRCMaker.Pages
         public Setup()
         {
             InitializeComponent();
+            PreviousButton.AddHandler(Button.MouseDownEvent, new RoutedEventHandler(NavigateToPrevious), true);
+            NextButton.AddHandler(Button.MouseDownEvent, new RoutedEventHandler(NavigateToNext), true);
+            PageInit();
             NavigateToWelcome();
+        }
+        Page[] SetupPages = new Page[5];
+        private void PageInit()
+        {
+            SetupPages[0] = new WelcomePage();
+            SetupPages[1] = new AccountSettingPage();
+            SetupPages[2] = new AccountSettingPage();
+            SetupPages[3] = new AccountSettingPage();
+            SetupPages[4] = new AccountSettingPage();
+        }
+        int PageNumber = 0;
+        private void NavigateToNext(object sender, RoutedEventArgs e)
+        {
+            if (PageNumber <= 3)
+            {
+                PageNumber++;
+                SetupFrame.Navigate(SetupPages[PageNumber]);
+                PreviousButtonContent.Text = PageNumber == 0 ? "了解更多" : "上一步";
+            }
+            NextButtonContent.Text = PageNumber == 4 ? "完成" : "下一步";
+        }
+        private void NavigateToPrevious(object sender, RoutedEventArgs e)
+        {
+            if (PageNumber != 0)
+            {
+                PageNumber--;
+                SetupFrame.Navigate(SetupPages[PageNumber]);
+                PreviousButtonContent.Text = PageNumber == 0 ? "了解更多" : "上一步";
+            }
+            NextButtonContent.Text = PageNumber == 4 ? "完成" : "下一步";
         }
         private void NavigateToWelcome()
         {
-            SetupFrame.Navigate(new WelcomePage());
+            SetupFrame.Navigate(SetupPages[0]);
         }
     }
 }
